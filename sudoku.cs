500756203882162e5c313369db3de8446d697766
muxammadxon202 <154244903+muxammadxon202@users.noreply.github.com>
2023-12-19 16:22:40 +0300

    add by Sirijiddinov

--- sudoku.cs
+++ sudoku.cs
@@ -125,4 +125,132 @@ public void SwapBlocksInRow()
             var block1 = r.Next(0, n);
             var block2 = r.Next(0, n);
             while (block1 == block2)
+block2 = r.Next(0, n);
+            block1 *= n;
+            block2 *= n;
+            for(int i = 0; i < n * n; i++)
+            {
+                var k = block2;
+                for(int j = block1; j < block1 + n; j++)
+                {
+                    var temp = map[j, i];
+                    map[j, i] = map[k, i];
+                    map[k, i] = temp;
+                    k++;
+                }
+            }
+        }
+        
+        public void SwapRowsInBlock()
+        {
+            Random r = new Random();
+            var block = r.Next(0, n);
+            var row1 = r.Next(0, n);
+            var line1 = block * n + row1;
+            var row2 = r.Next(0, n);
+            while (row1 == row2)
+                row2 = r.Next(0, n);
+            var line2 = block * n + row2;
+            for(int i = 0; i < n * n; i++)
+            {
+                var temp = map[line1, i];
+                map[line1, i] = map[line2, i];
+                map[line2, i] = temp;
+            }
+        }
+
+        public void SwapColumnsInBlock()
+        {
+            Random r = new Random();
+            var block = r.Next(0, n);
+            var row1 = r.Next(0, n);
+            var line1 = block * n + row1;
+            var row2 = r.Next(0, n);
+            while (row1 == row2)
+                row2 = r.Next(0, n);
+            var line2 = block * n + row2;
+            for (int i = 0; i < n * n; i++)
+            {
+                var temp = map[i,line1];
+                map[ i, line1] = map[i,line2];
+                map[i,line2] = temp;
+            }
+        }
+
+        public void MatrixTransposition()
+        {
+            int[,] tMap = new int[n * n, n * n];
+            for(int i = 0; i < n * n; i++)
+            {
+                for(int j = 0; j < n * n; j++)
+                {
+                    tMap[i, j] = map[j, i];
+                }
+            }
+            map = tMap;
+        }
+
+        public void CreateMap()
+        {
+            for (int i = 0; i < n * n; i++)
+            {
+                for (int j = 0; j < n * n; j++)
+                {
+                    Button button = new Button();
+                    buttons[i, j] = button;
+                    button.Size = new Size(sizeButton, sizeButton);
+                    button.Text = map[i, j].ToString();
+                    button.Click += OnCellPressed;
+                    button.Location = new Point(j * sizeButton, i * sizeButton);
+                    this.Controls.Add(button);
+                }
+            }
+        }
+
+        public void OnCellPressed(object sender, EventArgs e)
+        {
+            Button pressedButton = sender as Button;
+            string buttonText = pressedButton.Text;
+            if (string.IsNullOrEmpty(buttonText))
+            {
+                pressedButton.Text = "1";
+            }
+            else
+            {
+                int num = int.Parse(buttonText);
+                num++;
+                if (num == 10)
+                    num = 1;
+                pressedButton.Text = num.ToString();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < n * n; i++)
+            {
+                for(int j = 0; j < n * n; j++)
+                {
+                    var btnText = buttons[i, j].Text;
+                    if(btnText != map[i, j].ToString())
+                    {
+                        MessageBox.Show("Неверно!");
+                        return;
+                    }
+                }
+            }
+            MessageBox.Show("Верно!");
+            for(int i = 0; i < n * n; i++)
+            {
+                for (int j = 0; j < n * n; j++)
+                {
+                    this.Controls.Remove(buttons[i, j]);
+                }
+            }
+            GenerateMap();
+        }
+    }
+} 
+
 
